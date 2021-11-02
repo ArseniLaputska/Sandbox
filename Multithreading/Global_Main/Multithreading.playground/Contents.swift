50eb201c3cff50aef15366476c5a5c37a21cf295
Arseni <arsenii12345@yandex.ru>
2021-11-02 21:23:55 +0300

    Challenge with async, sync methods

--- Multithreading/Global_Main/Multithreading.playground/Contents.swift
+++ Multithreading/Global_Main/Multithreading.playground/Contents.swift
@@ -27,3 +27,54 @@ func fetchImage() {
 }
 
 fetchImage()
+
+func task1() {
+  print("task1")
+}
+func task2() {
+  print("task2")
+}
+func task3() {
+  print("task3")
+}
+func task4() {
+  print("task4")
+}
+func task5() {
+  print("task5")
+}
+func task6() {
+  print("task6")
+}
+func task7() {
+  print("task7")
+}
+func task8() {
+  print("task8")
+}
+
+// Task
+let serialQueue = DispatchQueue(label: "ru.arseni.serial-queue")
+let concurrentQueue = DispatchQueue(label: "ru.arseni.concurrent-queue", attributes: .concurrent)
+
+concurrentQueue.async {
+    task1()
+    
+    serialQueue.async(execute: task2)
+}
+
+concurrentQueue.sync {
+    serialQueue.sync(execute: task3)
+    
+    serialQueue.sync(execute: task4)
+    
+    serialQueue.async(execute: task5)
+}
+
+task6()
+
+serialQueue.async {
+    concurrentQueue.sync(execute: task7)
+}
+
+task8()
